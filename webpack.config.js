2ff9419e98a0715333429f90379e97532d6e5db2
MaxFung3t <332528284@qq.com>
2020-01-09 20:46:21 +0800

    提交代码

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,42 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin'); //该插件将CSS提取到单独的文件中
+const HtmlWebpackPlugin = require('html-webpack-plugin'); //自动引入打包生成得js
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");  //清除打包后的文件夹多余的文件
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin'); //压缩单独的css文件
+const PurifyCSSPlugin = require('purifycss-webpack'); //css优化去重复无效代码
+const glob = require('glob'); //css优化
 
 module.exports = {
     mode: 'development',
-    entry: path.resolve(__dirname, './src/index.js'),
+    entry: path.resolve(__dirname, './src/index.js'), //打包的入口文件地址
     output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: 'bundle.js'
+        path: path.resolve(__dirname, './dist'), //bundle 生成(emit)到哪里
+        filename: 'bundle.js' //生成 bundle 的名称
     },
-    plugins: [
+    plugins: [ //plugins 插件用于执行范围包括，从打包优化和压缩，一直到重新定义环境中的变量
         new MiniCssExtractPlugin({
-            filename: '[name].[hash:6].css' //对应css名字，6位数的hash数
+            filename: '[name].[hash:6].css' //生成css名字，6位数的hash数
         }),
+        new CleanWebpackPlugin(),
+        new OptimizeCssAssetsWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            minify: {
+                collapseWhitespace: true, //清除空格
+                removeAttributeQuotes: true, //清除多余引号
+                removeComments: true //删除注释
+
+            },
+            title: 'OutputManagement',
+            template: path.resolve(__dirname, './src/index.html'), //要打包的html文件路径
+            filename: 'index.html' //要打包的文件夹名字
+        }),
+        new PurifyCSSPlugin({
+            paths: glob.sync(path.join(__dirname, './src/*.html')),
+        })
     ],
     module: {
         rules: [
+            // loader 让 webpack 能够去处理那些非 JavaScript 文件（webpack 自身只理解 JavaScript）
             // style-loader 将 JS 字符串生成为 style 节点
             // css-loader 将 CSS 转化成 CommonJS 模块
             // sass-loader 将 Sass 编译成 CSS
@@ -26,12 +48,12 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                use: ['style-loader', 'css-loader','postcss-loader','sass-loader'],
+                use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
                 exclude: /node_modules/ //去除不必要的构建
             },
             {
                 test: /\.less$/,
-                use: ['style-loader', 'css-loader', 'postcss-loader','less-loader'],
+                use: ['style-loader', 'css-loader', 'postcss-loader', 'less-loader'],
                 exclude: /node_modules/ //去除不必要的构建
             },
             {
@@ -49,6 +71,12 @@ module.exports = {
                     }
                 }],
                 exclude: /node_modules/
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader'
+                ]
             },
             {
                 test: /\.js$/,
